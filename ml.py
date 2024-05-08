f2312b091cbcbb64fa5fd01230d9c152356c00e3
gurleen1997 <kaurgurleen280141@gmail.com>
2024-05-08 11:52:30 +0530

    added 3 new files

--- ml.py
+++ ml.py
@@ -1,1 +1,17 @@
 ml
+print('added new file ml into model')
+
+def model1():
+    print("adding code for model1")
+
+def model2():
+    print("adding code for model2")
+
+
+def model3():
+    print("adding code for model3")
+
+print("model 2 is performing best")
+
+
+
